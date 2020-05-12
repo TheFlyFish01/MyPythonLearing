c7f9c6794f980888a0a56580e1f7ac61af5f9a26
noob_01 <349077192@qq.com>
2020-05-12 11:36:40 +0800

    修改txt文件位置

--- 处理文件/txt.py
+++ 处理文件/txt.py
@@ -1,11 +1,11 @@
 def save_txt(name):
     #打开TXT文件，没有则创建
         #方式一
-    title_data1 = open("22.txt",'a',encoding='utf-8')
+    title_data1 = open("data/22.txt",'a',encoding='utf-8')
     title_data1.write("%s"%(name))
     title_data1.close()
         #方式二
-    with open("22.txt", "a+",encoding='utf-8') as fp:
+    with open("data/22.txt", "a+",encoding='utf-8') as fp:
         fp.write("%s\n"%(name))
         
 if __name__ == "__main__":
